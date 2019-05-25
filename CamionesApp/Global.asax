732350caa9566f17b95aa2a378bc909d10ce0981
Carlos López <carlos.armando.lopez.espinoza@uabc.edu.mx>
2019-05-25 12:55:59 -0500

    Agregar archivos de proyecto.

---
+++ CamionesApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CamionesApp.MvcApplication" Language="C#" %>
---
+++ CamionesApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CamionesApp.Startup))]
+namespace CamionesApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
